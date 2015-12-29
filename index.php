9288d1c99d83f1a29dd3e53a912068ba28c3fdf3
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-12-29 21:24:56 +0100

    index / glowna : Switched to Markdown

--- index.php
+++ index.php
@@ -23,26 +23,13 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 <div class="ogol">
 		<h1>Koło szachowe<br><font size="5"><div class="hright">przy IV Liceum Ogólnokształcącym im. Tadeusza Kościuszki w Toruniu</div></font></h1>
 		<!--<p class="mcenter"><a href="historia.html"><font size="5">Historia</font></a></p>-->
-		<?php include "menu.html"; ?>
-		<h2>Spotkania koła</h2>
-		<p>Spotkania koła odbywają się w co drugą środę (choć ostatnio od dłuższego czasu — w każdą); godzina rozpoczęcia między 14:30 a 15:10, godzina zakończenia zazwyczaj około 17 (czasem dłużej jak są jakieś długie partie).</p>
-		<p>Przeważnie jesteśmy w sali 11, 12, lub 13, zdarza się że w 14 (tak, tak!), w sytuacjach kryzysowych przy sklepiku w piwnicy bądź w pobliżu sal gimnastycznych czy przy schodach na oddział Gimnazjum. Raz po maturach, gdy koło formalnie już właściwie nie funkcjonuje, graliśmy na stołach Komisji (sic) (za pozwoleniem)!</p>
-		<p>Spotkania koła odbywają się pod opieką p. prof. Małgorzaty Petrowskiej. Do dyspozycji mamy siedem szachownic szkolnych + te które przyniesiemy (przynosimy również goban (Tomek Czarkowski) i szachownice do szachów trzyosobowych (heksagonalne — Józek Sędziak, papierowe do <a href="http://3manchess.com">3 Man Chess</a> — Michał Feiler).</p>
-		<p>Udział w spotkaniach koła jest zaliczany jako udział w zajęciach fakultatywnych z wychowania fizycznego.</p>
-
-		<h2>Co robimy na spotkaniach?</h2>
-		<p>Na spotkaniach m.in.:</p>
-		<ul>
-			<li>gramy w szachy, kto z kim chce</li>
-			<li>gramy w <a href="http://pl.wikipedia.org/wiki/Kloc">kloca</a></li>
-			<li>gramy w <a href="http://pl.wikipedia.org/wiki/Go">go</a></li>
-			<li>gramy w różne odmiany szachów i gry szachopodobne</li>
-			<li>gramy w <a href="http://3manchess.com">3 Man Chess</a>, głównie ze względu na <a href="http://github.com/ArchieT/3manchess">projekt na Toruński Festiwal Nauki i Sztuki</a> (nasze koło ma także swój wkład we współtworzeniu zasad tej gry)</li>
-			<li>uczymy grać w szachy osoby, które nie umieją grać</li>
-			<li>uczymy się różnych technik, komentujemy wzajemnie swoje rozgrywki</li>
-			<li>gadamy ☺</li>
-		</ul>
-		<p><b>Jeżeli masz jakiekolwiek pytania lub wątpliwości, <a href="mailto:szachyivlo@googlegroups.com">pisz na grupę mailową na <i>szachyivlo@googlegroups.com</i></a>, <a href="mailto:archiet@platinum.edu.pl">pisz do mnie na <i>archiet@platinum.edu.pl</i></a> lub <u>po prostu przyjdź na koło</u> ☺</b></p>
+		<?php
+include_once 'markdown.php';
+include "menu.html"; 
+$ourtext = file_get_contents('glowna.md');
+$ourcontent = Markdown($ourtext);
+echo $ourcontent;
+?>
 		
 		
 	</div>	
