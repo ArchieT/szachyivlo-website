3356f3b577ec4e2a4fb3d68275b221e6bc037f20
Michał Krzysztof Feiler <archiet@platinum.edu.pl>
2015-12-29 21:15:26 +0100

    historia: switched to Markdown

---
+++ historia.md
@@ -0,0 +1,21 @@
+## Historia
+
+### Pradawne dzieje
+
+Historia Koła sięga PRLu — wiadomo, że już około 1985 roku uczniowie "Czwórki"
+w ramach (lekcji? fakultetów?) wychowania fizycznego grywali w szachy. Wiadomo
+też o bardzo wysokich osiągnięciach szachistów z naszej szkoły na przestrzeni
+wielu lat. Ta sekcja zostanie rozwinięta, gdy uzyskamy więcej informacji.
+
+### Współczesność
+
+Koło (najprawdopodobniej) zachowywało było ciągłość od tamtych czasów, jednak
+zmniejszona aktywność poskutkowała rokiem przerwy, gdy p. prof. Petrowska
+poszła w r.szk. 2012/13 na urlop zdrowotny. Jednak świeża krew (Artur Szubarga
+razem z Kubą Wrzecionkowskim) z impetem reaktywowała je po jej powrocie
+wracając do normy 15-20 osób obecnych na spotkaniach; w związku z rozrostem
+koła zakupiono też szkolne szachownice.
+
+---------------------------------------
+
+**Wiesz coś więcej? Napisz do nas!**
--- historia.php
+++ historia.php
@@ -30,18 +30,14 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 <div class="ogol">
 		<h1>Koło szachowe<br><font size="5"><div class="hright">przy IV Liceum Ogólnokształcącym im. Tadeusza Kościuszki w Toruniu</div></font></h1>
 		<!--<p class="mcenter"><a href="index.html"><font size="5">Strona główna</font></a></p>-->
-<?php include "menu.html"; ?>
-		<h2 class="page">Historia</h2>
-		<h3>Pradawne dzieje</h3>
-		<p>Historia Koła sięga PRLu — wiadomo, że już około 1985 roku uczniowie "Czwórki" w ramach (lekcji? fakultetów?) wychowania fizycznego grywali w szachy. Wiadomo też o bardzo wysokich osiągnięciach szachistów z naszej szkoły na przestrzeni wielu lat. Ta sekcja zostanie rozwinięta, gdy uzyskamy więcej informacji.</p>
-		<h3>Współczesność</h3>
-		<p>Koło (najprawdopodobniej) zachowywało było ciągłość od tamtych czasów, jednak zmniejszona aktywność
-poskutkowała rokiem przerwy, gdy p. prof. Petrowska poszła w r.szk. 2012/13 na urlop zdrowotny.
-Jednak świeża krew (Artur Szubarga razem z Kubą Wrzecionkowskim) z impetem reaktywowała je po jej powrocie wracając do normy 15-20 osób obecnych na spotkaniach; w związku z rozrostem koła zakupiono też szkolne szachownice.
-</p>
-		<hr>
-		<p><b>Wiesz coś więcej? Napisz do nas!</b></p>
-		<br></div>
+<?php ]
+include_once 'markdown.php';
+include "menu.html"; 
+$ourtext = file_get_contents('historia.md');
+$ourconten = Markdown($ourtext);
+echo $ourconten;
+?>
+			<br></div>
 		<hr>
 		<div class="footer">Opiekun strony:<br>Michał Krzysztof Feiler <a href="mailto:archiet@platinum.edu.pl">&lt;archiet@platinum.edu.pl&gt;</a><br><a href="http://archiet.platinum.edu.pl">http://archiet.platinum.edu.pl</a></div>
 	</body>
